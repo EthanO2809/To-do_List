571e623bb6038cc010c04b0f3d4dc64e361165e3
Ethan Oliver <ethanliamoliver28@gmail.com>
2023-06-02 16:23:04 +0200

    todolist

--- Script/Script.js
+++ Script/Script.js
@@ -18,6 +18,28 @@ for (i = 0; i < close.length; i++) {
   }
 }
 
+// sort button
+function sortList() {
+    var list, i, switching, b, shouldSwitch;
+    list = document.querySelector('ul');
+    switching = true;
+    while (switching) {
+      switching = false;
+      b = list.getElementsByTagName("LI");
+      for (i = 0; i < (b.length - 1); i++) {
+        shouldSwitch = false;
+        if (b[i].innerHTML.toLowerCase() > b[i + 1].innerHTML.toLowerCase()) {
+          shouldSwitch = true;
+          break;
+        }
+      }
+      if (shouldSwitch) {
+        b[i].parentNode.insertBefore(b[i + 1], b[i]);
+        switching = true;
+      }
+    }
+  }
+
 // check symbol
 var list = document.querySelector('ul');
 list.addEventListener('click', function(ev) {
